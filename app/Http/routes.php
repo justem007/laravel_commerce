5dbb58d5edb76302a6e3a76ace29aeacceb99d4c
Ricardo Justem <ricardojustem@gmail.com>
2015-06-10 16:59:09 -0300

    First Commit

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,12 +10,111 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+//Route::get('users', function()
+//{
+//    return 'Users novos!';
+//});
+//
+//Route::get('my/page', function()
+//{
+//    return 'Hello Word!';
+//});
+//
+//Route::get('first', function()
+//{
+//    return 'First route.';
+//});
+//
+//Route::get('second', function()
+//{
+//    return 'Second route.';
+//});
+//
+//Route::get('first', function()
+//{
+//    return Redirect::to('second');
+//});
+//Route::get('second', function()
+//{
+//    return 'Second route';
+//});
+//
+//Route::get('third/page', function()
+//{
+//    return 'Potato';
+//});
 
-Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+
+//home page
+//Route::get('/', function(){});
+
+//routes for the books section
+//Route::get('/books', function() {
+//    return 'Books index';
+//});
+//Route::get('/books/fictions', function() {});
+//Route::get('/books/science', function() {});
+//Route::get('/books/romance', function() {});
+//
+////routes for the magazines section
+//Route::get('/magazines', function() {});
+//Route::get('/magazines/celebrity', function() {});
+//Route::get('/magazines/technology', function() {});
+
+//resoources/viwes
+//Route::get('/', function()
+//{
+//    return View::make('sample');
+//});
+//Route::get('/sample');
+
+//Route::get('/', 'WelcomeController@index');
+//
+//Route::get('home', 'HomeController@index');
+//
+//Route::controllers([
+//	'auth' => 'Auth\AuthController',
+//	'password' => 'Auth\PasswordController',
+//]);
+//Route::get('/{squirrel}', function($squirrel){
+//   $data['squirrel'] = $squirrel;
+//   return View::make('sample', $data);
+//});
+
+//Route::get('books', function(){
+//    if (Auth::guest()) return Redirect::to('login');
+//    //show books to only loggedin users.
+//});
+
+//Route::get('custom/response', function(){
+//    $response = Response::make('Hello word !', 200);
+//    $response->headers->set('our key', 'our value');
+//    return $response; 
+//});
+//
+//Route::get('our/response', function(){
+//    $response = Response::make('Bon, james Bold', 200);
+//    $response->setTtl(60);
+//    return $response; 
+//});
+//
+//Route::get('markdow/response', function(){
+//   $data = array('iron', 'man', 'rock');
+//   return Response::json($data);
+//});
+
+
+Route::filter('birthday', function(){
+    if (true) {
+        return View::make('birthday');
+    } 
+});
+
+Route::get('/', array(
+    'before' => 'birthday',
+    function()
+{
+    return View::make('hello');
+}));
---
+++ resources/views/birthday.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Happy Birthday !</h1>
+        <p>Happy birthday to Dayle, hurray !</p>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
